1b51aea861b9974699a7c941a50014fc16794e6d
rachana prajapati <rachanaprajapati1234@gmail.com>
2023-10-22 14:28:17 +1100

    template logo size

--- config/template.js
+++ config/template.js
@@ -537,7 +537,7 @@ exports.forgotPassword = function(result,callback)
                                                         background: linear-gradient(to right, #fff 0%, #fff 100%) !important;">
                                                           <!-- Your Logo -->
     
-                                                          <img align="center" alt="Logo" class="kmImage" src="http://3.26.203.80/api/logo192.png" width="50" style="border:0;height:"50";line-height:100%;outline:none;text-decoration:none;padding-bottom:0;display:inline;vertical-align:bottom;max-width:199px;" />
+                                                          <img align="center" alt="Logo" class="kmImage" src="http://3.26.203.80/api/logo192.png" width="50px" style="border:0;height:"50px";line-height:50%;outline:none;text-decoration:none;padding-bottom:0;display:inline;vertical-align:bottom;max-width:199px;" />
                                                         </td>
                                                       </tr>
                                                     </tbody>
