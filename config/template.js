78c48a2f92804454634c338700d78ff7b362bd71
rachana prajapati <rachanaprajapati1234@gmail.com>
2023-11-11 01:33:54 +1000

    changes in template file

--- config/template.js
+++ config/template.js
@@ -864,9 +864,9 @@ exports.forgotPassword = function(result,callback)
                                                           <p style="margin:0;padding-bottom:1em">Temporary Password : ${result.password} </p>
 
                                                           <a href="https://apps.apple.com/us/app/ballina-farm-fresh/id6470903718">
-                                                          <img align="center" alt="ios"  src="http://13.238.15.59/api/App.png" width="50%" height="130%"/></a>
+                                                          <img align="center" alt="ios"  src="http://13.238.15.59/api/App.png" width="35%" height="30%"/></a>
                                                           <a href="https://play.google.com/store/apps/details?id=com.app.balinas_veg_shop&pcampaignid=web_share">
-                                                          <img align="center" alt="android"  src="http://13.238.15.59/api/google.png" width="50%" height="50%" /></a>
+                                                          <img align="center" alt="android"  src="http://13.238.15.59/api/google.png" width="40%" height="50%" /></a>
 
                                                           <p style="margin:0;padding-bottom:1em"> If you did not request this change, please let us know by replying to this email.</p>
                                                           <p style="margin:0;padding-bottom:1em"> </p>
